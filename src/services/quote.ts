d336e6cce86bc82c80ef46d5df747c428035e91c
Carlos Ruiz <ruizcarlos93@yahoo.es>
2017-05-05 08:46:37 +0200

    Added quotes service

---
+++ src/services/quote.ts
@@ -0,0 +1,19 @@
+
+export class QuotesService{
+  private favoriteQuotes = [];
+
+  addQuoteToFavorites(quote){
+    this.favoriteQuotes.push(quote);
+  }
+
+  removeQuoteFromFavorites(quote){
+    const position = this.favoriteQuotes.findIndex((quoteElement) =>{
+      return quoteElement.id == quote.id;
+    });
+    this.favoriteQuotes.slpice(position,1);
+  }
+
+  getFavoriteQuotes(){
+    return this.favoriteQuotes.slice();
+  }
+}
